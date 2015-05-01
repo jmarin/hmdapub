b3cd4e8571784a5f64ea923e0540987e22a1645e
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-04-30 22:25:23 -0400

    Add file parsing test (jvm)

---
+++ hmdapub/jvm/src/test/scala/parser/LARFileParserTest.scala
@@ -0,0 +1,21 @@
+package parser
+
+import utest._
+
+
+object LARFileParserTest extends TestSuite {
+
+  def tests = TestSuite {
+    val source = scala.io.Source.fromFile("hmdapub/jvm/src/test/resources/lars2007.dat") 
+
+    val lines = source.getLines//.mkString("\n")
+    lines.foreach { line =>
+      val lar = LARParser(line) 
+      assert(lar.asOfDate == 2007)
+    }
+    source.close()
+
+  }
+}
+
+
